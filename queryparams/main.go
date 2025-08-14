569fd19d54435380dfe7e66e02619e1caa44a7d5
atharvamhaske <atharvamhaske76@gmail.com>
2025-08-14 13:35:47 +0530

    added more funcs

--- queryparams/main.go
+++ queryparams/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // http://api.example.com/api/v1/greet?name=Atharva
+
+//understanding query params
 func greetHandler( w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	name := query.Get("name")
@@ -16,8 +19,50 @@ func greetHandler( w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", name)
 }
 
+
+//extracting path variables
+// https://api.example.com/user/123
+// 1 -> User
+// 2 -> UserID
+
+func userHandler ( w http.ResponseWriter, r *http.Request) {
+	pathSegments := strings.Split(r.URL.Path, "/")
+	if len(pathSegments) >= 3 && pathSegments[1] == "user" {
+		userID := pathSegments[2]
+		name := pathSegments[3]
+		fmt.Fprintf(w, "User ID: %s\n", userID )
+		fmt.Fprintf(w, "Name is : %s", name )
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+//handling both
+// https://api.example.com/username/123?includeDetails=true
+
+func userDetailsHandler ( w http.ResponseWriter, r *http.Request ) {
+	pathSegments := strings.Split(r.URL.Path, "/")
+	query := r.URL.Query()
+	includeDetails := query.Get("includeDetails")
+
+	if len(pathSegments) >= 3 && pathSegments[1] == "username" {
+		userID := pathSegments[2]
+		response := fmt.Sprintf("User ID: %s", userID)
+
+		if includeDetails == "true" {
+			response += " (Details included )"
+		}
+		fmt.Fprintln(w, response)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+
 func main() {
 	http.HandleFunc("/greet", greetHandler)
+	http.HandleFunc("/user/", userHandler)
+	http.HandleFunc("/username/", userDetailsHandler)
 
 	fmt.Println("Listening at port 8080 ....")
 
